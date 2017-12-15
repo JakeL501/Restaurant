fbdd9e0c3a87a98e1e5ae799694819f8611c8d9f
emmapankey <emmapankey19@gmail.com>
2017-12-14 20:21:03 -0500

    exported route modules to server.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,13 +1,10 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+var waitinglistData = require("../data/waitinglistData.js");
+// console.log(waitinglistData);
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+var tableData = require("../data/tableData.js");
+// console.log(tableData);
+
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,19 +13,14 @@ app.use(bodyParser.json());
 // Routes
 // =============================================================
 
-app.get("/api/tables", function(req, res) {
+module.exports = function () {
+  app.get("/api/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tableData.js"));
-    
+
   });
 
-  app.get("/api/waitlist", function(req, res) {
+  app.get("/api/waitlist", function (req, res) {
     res.sendFile(path.join(__dirname, "waitinglistData.js"));
-        
-  });
 
-
-  // Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
   });
+}
